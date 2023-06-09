83327489640b969e52161e7e5bf3324b203f045a
danielro999 <persensus@yahoo.com.ar>
2023-06-08 22:23:59 -0300

    agregado funciones + - / * y operate

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const boton = document.getElementsByClassName("boton");
 const pantalla = document.getElementsByClassName("pantalla");
+const botones = ["reset", "del","%" ,"/",  7, 8, 9, "X", 4, 5, 6, "-", 1, 2, 3, "+", 0, ".", "+/-", "=" ]
 
-const botones = ["reset", "del", ,"/",  7, 8, 9, "X", 4, 5, 6, "-", 1, 2, 3, "+", 0, ".", "+/-", "=" ]
+
 for (let i = 0; i < boton.length; i++) {
     boton[i].textContent = botones[i]	 
     boton[i].onmouseover = function (){
@@ -11,10 +12,42 @@ for (let i = 0; i < boton.length; i++) {
         boton[i].style.backgroundColor = "#51c794";
     }  
     boton[i].onmousedown = function (){
-        boton[i].style.fontSize= "large";
+        boton[i].style.fontSize = "large";
+        pantalla[0].textContent += boton[i].textContent
     }   
     boton[i].onmouseup = function (){
         boton[i].style.fontSize= "x-large";
     } 
 }
 
+
+function add(a, b){
+    return a + b
+}
+
+function subtract (a, b){
+    return a - b 
+}
+
+function multiply (a, b){
+    return a * b
+}
+
+function divide (a, b) {
+    return a / b
+}
+
+function operate (a, b, operator){
+    if (operator == "+"){
+        return add(a, b)
+    }
+    if (operator == "-"){
+        return subtract(a, b)
+    }
+    if (operator == "*"){
+        return multiply(a, b)
+    }
+    if (operator == "/"){
+        return divide(a, b)
+    }
+}
