46acd2f6ed69aac341674e9c0013cbf816db953d
danielro999 <persensus@yahoo.com.ar>
2023-06-12 14:35:59 -0300

    cambiado el nombre boton a arraybotones

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-const boton = document.getElementsByClassName("boton");
+const arrayBotones = document.getElementsByClassName("boton");
 const pantalla = document.getElementsByClassName("pantalla");
 const botones = ["reset", "del","%" ,"/",  7, 8, 9, "X", 4, 5, 6, "-", 1, 2, 3, "+", 0, ".", "+/-", "=" ]
 
-
-for (let i = 0; i < boton.length; i++) {
-    boton[i].textContent = botones[i]	 
-    boton[i].onmouseover = function (){
-        boton[i].style.backgroundColor = "#238c5e";
+for (let i = 0; i < arrayBotones.length; i++) {
+    arrayBotones[i].textContent = botones[i]	
+     
+    arrayBotones[i].onmouseover = function (){
+        arrayBotones[i].style.backgroundColor = "#238c5e";
     }   
-    boton[i].onmouseout = function (){
-        boton[i].style.backgroundColor = "#51c794";
+    arrayBotones[i].onmouseout = function (){
+        arrayBotones[i].style.backgroundColor = "#51c794";
     }  
-    boton[i].onmousedown = function (){
-        boton[i].style.fontSize = "large";
-        pantalla[0].textContent += boton[i].textContent
+    arrayBotones[i].onmousedown = function (){
+        arrayBotones[i].style.fontSize = "large";
+        pantalla[0].textContent += arrayBotones[i].textContent
     }   
-    boton[i].onmouseup = function (){
-        boton[i].style.fontSize= "x-large";
+    arrayBotones[i].onmouseup = function (){
+        arrayBotones[i].style.fontSize= "x-large";
     } 
 }
 
